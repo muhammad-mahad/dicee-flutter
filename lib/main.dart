ff6b9111bc47abc34b2c449f4c179f1433e089ab
zfighters <62249047+zfighters@users.noreply.github.com>
2021-09-04 16:54:40 +0500

    Completed!

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'dart:math';
 
 void main() {
   return runApp(
@@ -15,9 +17,58 @@ void main() {
   );
 }
 
-class DicePage extends StatelessWidget {
+class DicePage extends StatefulWidget {
+
+  @override
+  _DicePageState createState() => _DicePageState();
+}
+
+class _DicePageState extends State<DicePage> {
+  int dice1Value = 2;
+  int dice2Value = 5;
+
+  void generateNumber(){
+    setState((){
+      dice1Value = Random().nextInt(6) + 1;
+      dice2Value = Random().nextInt(6) + 1;
+    }
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+      child: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    padding: EdgeInsets.all(16.0),
+                  ),
+                    onPressed: () => generateNumber(),
+                    child: Image.asset("images/dice${dice1Value}.png"),
+                ),
+              ),
+            ),
+            Expanded(
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: TextButton(
+                  style: TextButton.styleFrom(
+                    padding: EdgeInsets.all(16.0),
+                  ),
+                  onPressed: () => generateNumber(),
+                  child: Image.asset("images/dice${dice2Value}.png"),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
